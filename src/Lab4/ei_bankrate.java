70c51bd910c364fe8c335e3714a6362bd215f4a9
Le Truong Nguyen <nguyen.letruong.cit22@eiu.edu.vn>
2026-01-29 15:21:16 +0700

    Nop ai_bankrate

--- src/Lab4/ei_bankrate.java
+++ src/Lab4/ei_bankrate.java
@@ -6,6 +6,7 @@ public class ei_bankrate {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		double money = sc.nextDouble();
 		int month = sc.nextInt();
 		double monthrate = money * month * (0.09 / 12);
